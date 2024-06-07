f65f9817f386c7485cf34b636cf74a56a7325eb9
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-06-07 16:29:37 +0530

    weather project is created.

--- BackEnd/calculator/calculator.js
+++ BackEnd/calculator/calculator.js
@@ -2,12 +2,17 @@ const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function (req, res) {
+app.post("/calculator", function (req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
 
@@ -15,6 +20,15 @@ app.post("/", function (req, res) {
 
   res.send("Answer ;" + result);
 });
+
+app.post("/bmicalculator", function (req, res) {
+  var height = parseFloat(req.body.height);
+  var weight = parseFloat(req.body.weight);
+
+  var bmi = weight / height ** 2;
+  res.send("Your BMI value is: " + bmi);
+});
+
 app.listen(4000, function () {
   console.log("Serever is running on port 4000.");
 });
