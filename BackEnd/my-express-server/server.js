77f704d4acdcc1204a78fe2ffce0899a05c9cc43
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-06-07 10:11:01 +0530

    create simple calculator

--- BackEnd/my-express-server/server.js
+++ BackEnd/my-express-server/server.js
@@ -6,6 +6,17 @@ app.get("/", function (req, res) {
   res.send("<h1> Hello world </h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("contacy me: ashen60438@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send("name: ashen");
+});
+
+app.get("/help", function (req, res) {
+  res.send("support help me.");
+});
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
