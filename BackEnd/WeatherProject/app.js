e07d2a2b4f7a17d28bee1cc8390123b52b98103a
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-06-07 22:09:34 +0530

    built get and post request for weather at user given location

--- BackEnd/WeatherProject/app.js
+++ BackEnd/WeatherProject/app.js
@@ -1,13 +1,42 @@
+const { log } = require("console");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req, res){
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-    https.get("")
-    res.send("Server is up and running.");
-})
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "cb8ae455e1cefb1b8f166de23b539b05";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=" +
+    apiKey +
+    "&q=" +
+    query +
+    "&units=" +
+    unit;
+
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDiscription = weatherData.weather[0].discription;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write("<h1>The Temperature in"+query+"is = " + temp + " </h1>");
+      res.write("Today is like" + weatherDiscription);
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    });
+  });
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
